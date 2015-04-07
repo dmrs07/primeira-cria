01612598f9154898f11dd8a4726a064ceae53b80
Fabio Quaresma <fabioquaresma@gmail.com>
2015-04-07 20:22:23 -0300

    Port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var http = require('http');
 var app  = require('./config/express')(app);
 
-http.createServer(app).listen(app.get('port'));
+http.createServer(app).listen(process.env.PORT || 5000);
