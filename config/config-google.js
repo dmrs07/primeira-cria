55331937f6e2d1ba02d8d798f5f46f6575336994
Daniel Moraes <dmrs07@gmail.com>
2015-04-13 09:49:19 -0300

    Update config-google.js

--- config/config-google.js
+++ config/config-google.js
@@ -1,6 +1,4 @@
 module.exports={
-  "GOOGLE_CLIENT_ID"      :     "615820119310-kjbm4p7018ir0l5aqldd3mib5idnh2rr.apps.googleusercontent.com",
-  "GOOGLE_CLIENT_SECRET"  :     "Um9o_bFGYpKjRldiMhhli2eT"
-  //"GOOGLE_CLIENT_ID"      :     "615820119310-cqualgb19g5ad1sdbk3d1977b25plkdv.apps.googleusercontent.com",
-  //"GOOGLE_CLIENT_SECRET"  :     "oYOurXhe9M_GP04r681CMYlA"
+  "GOOGLE_CLIENT_ID"      :     "615820119310-cqualgb19g5ad1sdbk3d1977b25plkdv.apps.googleusercontent.com",
+  "GOOGLE_CLIENT_SECRET"  :     "oYOurXhe9M_GP04r681CMYlA"
 }
