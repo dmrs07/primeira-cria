7af9fb3e17c164ac5a5b605a7b2df122969aee2c
Daniel Moraes <dmrs07@gmail.com>
2015-04-21 22:41:33 -0300

    callback google remote

--- config/config-google.js
+++ config/config-google.js
@@ -1,5 +1,6 @@
 module.exports={
   "GOOGLE_CLIENT_ID"      :     "615820119310-cqualgb19g5ad1sdbk3d1977b25plkdv.apps.googleusercontent.com",
   "GOOGLE_CLIENT_SECRET"  :     "oYOurXhe9M_GP04r681CMYlA",
-  "GOOGLE_CALLBACK_URL"   :     "http://localhost:5000/auth/google/callback"
+//  "GOOGLE_CALLBACK_URL"   :     "http://localhost:5000/auth/google/callback"
+  "GOOGLE_CALLBACK_URL"   :     "http://www.primeiracria.com.br/auth/google/callback"
 }
