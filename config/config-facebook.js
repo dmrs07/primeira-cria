60463ce42cf2dbcfb6e3ec881d4b8ff45359db4d
Daniel Moraes <dmrs07@gmail.com>
2016-12-27 02:10:46 -0200

    Facebook key

--- config/config-facebook.js
+++ config/config-facebook.js
@@ -1,6 +1,6 @@
 module.exports={
-  "FACEBOOK_API_KEY"      :     "1645095605713781",
-  "FACEBOOK_API_SECRET"   :     "8d928b4211acc2303bd1859120e156b4",
+  "FACEBOOK_API_KEY"      :     "647143395473805",
+  "FACEBOOK_API_SECRET"   :     "ce33af3129c80f6f14972d1f4472b59a",
 //  "CALLBACK_URL"          :     "http://localhost:5000/auth/facebook/callback"
   "CALLBACK_URL"          :     "http://www.primeiracria.com.br/auth/facebook/callback"
 }
